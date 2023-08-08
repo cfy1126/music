d71358fbd4f5a381fd4668682e78ba4d96934d7b
linkincao <linkincao1988@gmail.com>
2023-08-08 22:32:54 +0800

    show playlist

---
+++ cloudfunctions/music/index.js
@@ -0,0 +1,18 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env: 'develop-1gql0wjg71ab156f',
+})
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  return await cloud.database().collection('playlist')
+    .skip(event.start)
+    .limit(event.count)
+    .orderBy('createTime', 'desc')
+    .get()
+    .then(res => {
+      return res
+    })
+}
